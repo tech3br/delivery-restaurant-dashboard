bfc8f8ceeb11df4ed518dbf9536551d97dacc9cd
tech3br <dfsdireito95@gmail.com>
2022-11-18 12:32:47 -0300

    refact: refactoring main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { ThemeProvider } from 'styled-components';
-import { Header } from './components/Header/Header';
+import { Home } from './pages/Home';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 
 export function App() {
-
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Header />
-        <GlobalStyles />
+        <DndProvider backend={HTML5Backend}>
+          <Home />
+          <GlobalStyles />
+        </DndProvider>
       </ThemeProvider>
     </>
   );
