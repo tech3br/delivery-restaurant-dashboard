81b0c79f3e687c33b2df1b7c31b1ed663cbc41b7
tech3br <dfsdireito95@gmail.com>
2022-11-18 12:32:09 -0300

    refactor: refactoring global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,29 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Nunito, sans-serif;
+  }
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Nunito, sans-serif;
-}
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
 
-html, body, #root {
-  width: 100%;
-  height: 100%;
-}
+  body {
+    background: ${(props) => props.theme.colors.white};
+    color: ${(props) => props.theme.colors.gray500};
+  }
 
-body {
-  background: ${(props) => props.theme.colors.white};
-  color: ${(props) => props.theme.colors.gray500};
-
-}
-
-button {
-  cursor: pointer;
-  font-size: 1rem;
-  color: ${(props) => props.theme.colors.gray500};
-}
-
+  button {
+    cursor: pointer;
+    font-size: 1rem;
+    color: ${(props) => props.theme.colors.gray500};
+  }
 `;
