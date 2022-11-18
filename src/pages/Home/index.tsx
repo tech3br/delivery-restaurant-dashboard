024206b922695bc5478606db2d7d3f8639b75513
tech3br <dfsdireito95@gmail.com>
2022-11-18 12:32:23 -0300

    feat: adding home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header/';
+import { Orders } from '../../components/Orders';
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Orders />
+    </>
+  );
+}
